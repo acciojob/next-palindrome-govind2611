cc571c998ec905291f1f871de0365d9dd87c635c
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-06-15 11:37:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function nextPalindrome(num) {
-  //your JS code here.
+  num = parseInt(num) + 1; // Increment the number to start checking the next one
+
+  while (true) {
+    if (isPalindrome(num)) {
+      return num;
+    }
+    num++;
+  }
+}
+
+function isPalindrome(num) {
+  // Convert the number to a string
+  const str = num.toString();
+
+  // Check if the string is equal to its reverse
+  return str === str.split("").reverse().join("");
 }
 
 const input = prompt("Enter a palindrome number");
